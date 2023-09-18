31d3b362e0373ef5ddeac17cf43591bfdb25c0e3
Kattis91 <katya.svedmark@gmail.com>
2023-09-18 14:35:16 +0000

    Create a new form to be able to submit tasks to the database

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,9 +2,19 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
     var sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-  });
 
-  document.addEventListener('DOMContentLoaded', function() {
+    // modal initialization
     var modal = document.querySelectorAll('.modal');
     M.Modal.init(modal);
-  });
+  
+    // datepicker initialization
+    var datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+    });
+
+    // select initialization
+    var selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+});
